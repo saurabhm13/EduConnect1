91fd294c8380b9a3082d1126bbf2eb08d777d0d2
Saurabh <saurabhm1309@gmail.com>
2023-09-13 09:03:19 +0530

    Home Fragment UI Created

--- app/src/main/java/com/example/educonnect/ui/activity/MainActivity.kt
+++ app/src/main/java/com/example/educonnect/ui/activity/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.example.educonnect
+package com.example.educonnect.ui.activity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.educonnect.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
--- app/src/main/java/com/example/educonnect/ui/activity/MobileLoginActivity.kt
+++ app/src/main/java/com/example/educonnect/ui/activity/MobileLoginActivity.kt
@@ -1,4 +1,4 @@
-package com.example.educonnect
+package com.example.educonnect.ui.activity
 
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
