6012ac3929e374d6ddd693f81aa9dad8892dda6f
Saurabh <saurabhm1309@gmail.com>
2023-09-13 19:01:06 +0530

    Chat and Profile Fragment UI Created

---
+++ app/src/main/java/com/example/educonnect/ui/activity/SingleChatActivity.kt
@@ -0,0 +1,12 @@
+package com.example.educonnect.ui.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.educonnect.R
+
+class SingleChatActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_single_chat)
+    }
+}
