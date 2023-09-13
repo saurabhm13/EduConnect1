4a48f9321715a2a1a52cf26752300256d2b4a65a
Saurabh <saurabhm1309@gmail.com>
2023-09-13 19:28:40 +0530

    Video and Article Activity UI Created

---
+++ app/src/main/java/com/example/educonnect/ui/activity/VideoActivity.kt
@@ -0,0 +1,12 @@
+package com.example.educonnect.ui.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.educonnect.R
+
+class VideoActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_video)
+    }
+}
